fb7c0deaa724bf2bdbbd70abe596670f9ad5a667
Vlad Prodan <vladprodan6@gmail.com>
2022-10-18 16:26:43 +0200

    menu-config & render menu items

---
+++ app/_mock/menu-config.ts
@@ -0,0 +1,263 @@
+export const menus = [
+    {
+        name: 'Home',
+        children: [
+            {
+                href: '/',
+                name: 'Home Agency',
+            },
+            {
+                href: '/',
+                name: 'Home Studio',
+            },
+            {
+                href: '/',
+                name: 'Home Freelancer',
+            },
+            {
+                href: '/',
+                name: 'Home Developer',
+            },
+            {
+                href: '/',
+                name: 'Home Blogger',
+            },
+            {
+                href: '/',
+                name: 'Home Coach',
+            },
+            {
+                href: '/',
+                name: 'Business Startup',
+            },
+            {
+                href: '/',
+                name: 'Marketing Agency',
+            },
+            {
+                href: '/',
+                name: 'Business Consulting',
+            },
+            {
+                href: '/',
+                name: 'Creative Agency',
+            },
+            {
+                href: '/',
+                name: 'Creative Agency 2',
+            },
+            {
+                href: '/',
+                name: 'Home Developer',
+            },
+            {
+                href: '/',
+                name: 'Home Portfolio 2',
+            },
+        ]
+    },
+    {
+        name: 'About',
+        children: [
+            {
+                href: '/',
+                name: 'About Agency',
+            },
+            {
+                href: '/',
+                name: 'About Business',
+            },
+            {
+                href: '/',
+                name: 'About Me',
+            },
+        ]
+    },
+    {
+        name: 'Portfolio',
+        children: [
+            {
+                href: '/',
+                name: 'Porfolio Parallax',
+            },
+            {
+                href: '/',
+                name: 'Porfolio Horizontal Scroll',
+            },
+            {
+                href: '/',
+                name: 'Porfolio Masonary',
+            },
+            {
+                href: '/',
+                name: 'Porfolio Single V1',
+            },
+            {
+                href: '/',
+                name: 'Porfolio Single V2',
+            },
+            {
+                href: '/',
+                name: 'Porfolio Single V3',
+            },
+        ]
+    },
+    {
+        name: 'Service',
+        children: [
+            {
+                href: '/',
+                name: 'Service V1',
+            },
+            {
+                href: '/',
+                name: 'Service V2',
+            },
+            {
+                href: '/',
+                name: 'Service V3',
+            },
+        ]
+    },
+    {
+        name: 'Team',
+        children: [
+            {
+                href: '/',
+                name: 'Team',
+            },
+            {
+                href: '/',
+                name: 'Career',
+            },
+            {
+                href: '/',
+                name: 'Career Details',
+            },
+            {
+                href: '/',
+                name: 'Pricing',
+            },
+            {
+                href: '/',
+                name: '404',
+            },
+            {
+                href: '/',
+                name: 'Coming Soon',
+            },
+        ]
+    },
+    {
+        name: 'Blog',
+        children: [
+            {
+                href: '/',
+                name: 'Blog Layout V1',
+            },
+            {
+                href: '/',
+                name: 'Blog Layout V2',
+            },
+            {
+                href: '/',
+                name: 'Blog Single Layout V1',
+            },
+            {
+                href: '/',
+                name: 'Blog Single Layout V2',
+            },
+        ]
+    },
+    {
+        name: 'Contact',
+        children: [
+            {
+                href: '/',
+                name: 'Contact V1',
+            },
+            {
+                href: '/',
+                name: 'Contact V1',
+            },
+        ]
+    },
+    {
+        name: 'Elements',
+        children: [
+            {
+                href: '/',
+                name: 'Counter',
+            },
+            {
+                href: '/',
+                name: 'Blockquote',
+            },
+            {
+                href: '/',
+                name: 'Partners',
+            },
+            {
+                href: '/',
+                name: 'Pricing',
+            },
+            {
+                href: '/',
+                name: 'Newsletter',
+            },
+            {
+                href: '/',
+                name: 'Contact',
+            },
+            {
+                href: '/',
+                name: 'Team',
+            },
+            {
+                href: '/',
+                name: 'Blog',
+            },
+            {
+                href: '/',
+                name: 'Footer',
+            },
+            {
+                href: '/',
+                name: 'Portfolio',
+            },
+            {
+                href: '/',
+                name: 'Testimonials',
+            },
+            {
+                href: '/',
+                name: 'Header',
+            },{
+                href: '/',
+                name: 'Services',
+            },
+            {
+                href: '/',
+                name: 'About Us',
+            },{
+                href: '/',
+                name: 'Awards',
+            },
+            {
+                href: '/',
+                name: 'Events',
+            },{
+                href: '/',
+                name: 'Features',
+            },
+            {
+                href: '/',
+                name: 'Hero',
+            },
+            {
+                href: '/',
+                name: 'Slider',
+            },
+        ]
+    },
+]
